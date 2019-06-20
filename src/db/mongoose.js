d075c225a4ae02ebf6ff248cc1e1587d9d06abf0
takumab <takumab@gmail.com>
2019-06-20 16:30:16 +0100

    Remove unnecessary code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,12 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/ft_organisations', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('successfully connected');
-});
-
-export default db;
